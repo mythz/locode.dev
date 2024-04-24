cc2fbf219b0bc6e1217b933b8f59fb8bffc0b7e3
Demis Bellot <demis.bellot@gmail.com>
2024-04-24 10:37:44 +0800

    Update QuestionPost.tsx

--- src/components/QuestionPost.tsx
+++ src/components/QuestionPost.tsx
@@ -9,24 +9,25 @@ type Props = {
 }
 
 export default ({ question }: Props) => {
-    const pvqUrl = `https://pvq.app/questions/${question.id}/${question.post.slug}`
+    const post = question!.post
+    const pvqUrl = `https://pvq.app/questions/${question.id}/${post.slug}`
     const now = new Date()
     return (
         <div className="mb-20" data-module="mjs/question.mjs">
-            <article id="question" data-postid={question.id} data-createdby={question.post.createdBy}>
+            <article id="question" data-postid={question.id} data-createdby={post.createdBy}>
                 <h1 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-100 lg:text-4xl xl:text-5xl">
-                    <span className="block xl:inline">{question.post.title}</span>
+                    <span className="block xl:inline">{post.title}</span>
                 </h1>
                 <div className="my-4 py-2 flex justify-between border-b border-gray-200 dark:border-gray-700">
                     <div className="flex gap-x-6 text-sm text-gray-500 dark:text-gray-400">
                         <div>
                             <a className="text-indigo-700 dark:text-indigo-200 hover:underline" href={pvqUrl}>asked</a>
-                            <b className="ml-1">{relativeTimeFromDate(question?.post.creationDate)}</b>
+                            <b className="ml-1">{relativeTimeFromDate(post.creationDate)}</b>
                         </div>
-                        {question?.post.lastEditDate != null && question?.post.lastEditDate !== question?.post.creationDate
+                        {post.lastEditDate != null && post.lastEditDate !== post.creationDate
                             ? (<div>
                                 <span>last updated</span>
-                                <b className="ml-1">{relativeTimeFromDate(question?.post.lastEditDate)}</b>
+                                <b className="ml-1">{relativeTimeFromDate(post.lastEditDate)}</b>
                             </div>)
                             : null}
 
@@ -38,7 +39,7 @@ export default ({ question }: Props) => {
                             : null}
                     </div>
                 </div>
-                <div id="@Question.Post.Id" className="flex">
+                <div id="@post.Id" className="flex">
                     <div className="md:w-32 pr-2">
                         <div data-refid={question.id} className="voting flex flex-col items-center pr-2">
                             <svg className="up w-6 h-6 sm:w-10 sm:h-10 cursor-pointer select-none hover:text-green-600" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
@@ -50,15 +51,15 @@ export default ({ question }: Props) => {
                             </svg>
                         </div>
 
-                        {question?.post.createdBy
+                        {post.createdBy
                             ? (<div className="my-4 flex flex-col items-center text-sm">
-                                <img className="w-10 h-10 xl:w-16 xl:h-16 bg-cover inline-block" src={getAvatarUrl(question.post.createdBy)} />
-                                <div className="mt-1 hidden md:block text-center whitespace-nowrap font-semibold">{question.post.createdBy}</div>
+                                <img className="w-10 h-10 xl:w-16 xl:h-16 bg-cover inline-block" src={getAvatarUrl(post.createdBy)} />
+                                <div className="mt-1 hidden md:block text-center whitespace-nowrap font-semibold">{post.createdBy}</div>
                                 <div className="mt-1 flex items-center">
                                     <svg className="h-4 w-4 text-yellow-500" viewBox="0 0 32 32">
                                         <circle cx="16" cy="16" r="8" fill="currentColor" />
                                     </svg>
-                                    <span className="font-semibold" data-rep-user={question.post.createdBy}>1</span>
+                                    <span className="font-semibold" data-rep-user={post.createdBy}>1</span>
                                 </div>
                             </div>)
                             : null}
@@ -67,7 +68,7 @@ export default ({ question }: Props) => {
                     </div>
                     <div>
                         <div id={`preview-${question.id}`} className="pb-6 preview xl:flex-grow prose">
-                            {renderMarkdown(question.post.body)}
+                            {renderMarkdown(post.body)}
                         </div>
                         <div id={`edit-${question.id}`} className="edit w-full hidden"></div>
 
@@ -75,12 +76,12 @@ export default ({ question }: Props) => {
                             <div className="flex-grow">
                                 <div className="flex space-x-4 divide-x divide-gray-200 dark:divide-gray-800 text-sm sm:space-x-6 w-full">
                                     <div className="flex flex-wrap gap-x-2 gap-y-2">
-                                        {(question?.post.tags ?? []).map(tag => (
+                                        {(post.tags ?? []).map(tag => (
                                             <a href={`questions/tagged/${encodeURIComponent(tag)}`} className="inline-flex items-center rounded-md bg-blue-50 dark:bg-blue-900 hover:bg-blue-100 dark:hover:bg-blue-800 px-2 py-1 text-xs font-medium text-blue-700 dark:text-blue-200 ring-1 ring-inset ring-blue-700/10">{tag}</a>
                                         ))}
                                     </div>
                                 </div>
-                                {question.post.lockedDate
+                                {post.lockedDate
                                     ? (<div data-question={question.id} className="relative mt-4 text-sm">
                                         <span className="share-link mr-2 cursor-pointer select-none text-indigo-700 dark:text-indigo-300 hover:text-indigo-500" title="Share this Question">share</span>
                                         <span className="edit-link mr-2 cursor-pointer select-none text-indigo-700 dark:text-indigo-300 hover:text-indigo-500" title="Edit this Question">edit</span>
@@ -90,13 +91,13 @@ export default ({ question }: Props) => {
                             </div>
                             <div className="ml-2 text-xs">
                                 <div className="flex">
-                                    <span>{!question.post.lastEditDate ? "created" : "edited"}</span>
+                                    <span>{!post.lastEditDate ? "created" : "edited"}</span>
                                     <dd className="ml-1 text-gray-600 dark:text-gray-300">
-                                        <time datetime={getModifiedDate(question?.post.lastEditDate)}>{formatDate(getModifiedDate(question.post))}</time>
-                                        {question.post.modifiedBy && question.post.modifiedBy !== question.post.createdBy
+                                        <time datetime={getModifiedDate(post)}>{formatDate(getModifiedDate(post))}</time>
+                                        {post.modifiedBy && post.modifiedBy !== post.createdBy
                                             ? (<span>
                                                 <span> by </span>
-                                                <b>{question.post.modifiedBy}</b>
+                                                <b>{post.modifiedBy}</b>
                                             </span>)
                                             : null}
                                     </dd>
@@ -104,7 +105,7 @@ export default ({ question }: Props) => {
                             </div>
                         </div>
                         <div className="pb-6 flex items-end w-full">
-                            {question?.post.lockedDate
+                            {post.lockedDate
                                 ? (<div className="mt-4 w-full border-l-4 border-red-400 bg-red-50 p-4">
                                     <div className="flex w-full">
                                         <div className="flex-shrink-0">
@@ -115,7 +116,7 @@ export default ({ question }: Props) => {
                                         <div className="ml-3">
                                             <p className="text-sm text-red-700">
                                                 This question is locked.
-                                                <span className="ml-1 font-semibold">{question.post.lockedReason}</span>
+                                                <span className="ml-1 font-semibold">{post.lockedReason}</span>
                                             </p>
                                         </div>
                                     </div>
